f7cddf69e768a5f58d7487985af860246a3b5524
jpalegria <jalegria.trabajo@gmail.com>
2022-12-08 10:24:19 -0300

    feature: It adds route definitions for app

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,14 @@
 <?php
 
+use App\Simulators\TennisGame;
+use App\Simulators\TennisMatch;
+use App\Simulators\TennisSet;
+use App\Simulators\TennisTournament;
+use App\Http\Controllers\PlayerController;
+use App\Http\Controllers\TournamentController;
+use App\Models\Player;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Route as RoutingRoute;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +22,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+/**
+ * HEALTH CHECK ENDPOINTS
+ */
+Route::get('/ping', function (Request $request) {
+    return response('{"code":200,"message":"pong"}');
 });
+
+/**
+ * PLAYER ENDPOINTS
+ */
+Route::get('/players', [PlayerController::class, 'index']);
+Route::get('/players/{id}', [PlayerController::class, 'show']);
+Route::post('/players', [PlayerController::class, 'store']);
+Route::delete('/players/{id}', [PlayerController::class, 'destroy']);
+
+/**
+ * TOURNAMENT ENDPOINTS
+ */
+Route::get('/tournaments', [TournamentController::class, 'index']);
+Route::get('/tournaments/{id}', [TournamentController::class, 'show']);
+Route::post('/tournaments', [TournamentController::class,'store']);
+Route::put('/tournaments/{id}/simulate', [TournamentController::class,'play']);
